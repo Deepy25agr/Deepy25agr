e71c2eca01551019beb01b8bacd5beabde4f62db
Deepy25agr <106154384+Deepy25agr@users.noreply.github.com>
2022-05-24 16:39:20 +0530

    Add files via upload

---
+++ arithmetic_operator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"num1 ";
+    cin>>num1;
+
+    cout<<"num2 ";
+    cin>>num2;
+
+    cout<<"the sum is "<<num1+num2<<endl;
+
+    cout<<"the subtraction is "<<num1-num2<<endl;
+
+    cout<<"the multiplication is "<<num1*num2<<endl;
+
+    cout<<"the division is "<<num1/num2<<endl;
+
+    cout<<"the modulus is "<<num1%num2<<endl;
+
+    return 0;
+}
